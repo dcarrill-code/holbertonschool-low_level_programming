ca67854d47f5bce042cd87498c51b8167cc20eec
dcarrill-code <3346@holbertonschool.com>
2021-06-17 19:35:07 +0000

    First try

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - This main show some text too
+* Description: This C code show some text that i need whit p* rintf
+* Return: Returns a string whit the text needed
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
