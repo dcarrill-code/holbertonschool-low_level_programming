dd7860885ce8e6d9993d4622a99e34a3bf5c5e1d
dcarrill-code <3346@holbertonschool.com>
2021-06-17 19:18:10 +0000

    Fixing the c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>/*This line import the stdio lib*/
 #include <string.h>/*This line import the string lib*/
+/**
+* main - return some text
+* Description: This C code return the string that i need.
+* Return: A string that has the text needed
+*/
 int main(void)
 {
+
 char string[52];
 
 strcpy(string, "\"Programming is like building a multilingual puzzle");
