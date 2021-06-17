0b38bf02da06c817e7df0a0f7b7389fa812ec845
dcarrill-code <3346@holbertonschool.com>
2021-06-17 18:29:22 +0000

    adding ;

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>/*This line import the string lib*/
 int main(void){
   char string[20];
-  strcpy(string, "Programming is like building a multilingual puzzle")
+  strcpy(string, "Programming is like building a multilingual puzzle");
 
   puts(string);
   return 0;
