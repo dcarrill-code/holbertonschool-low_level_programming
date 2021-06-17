65f71fd50c084953eea1d82c70be5431fbcf08ac
dcarrill-code <3346@holbertonschool.com>
2021-06-17 18:49:33 +0000

    Fixing betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>/*This line import the stdio lib*/
 #include <string.h>/*This line import the string lib*/
-int main(void){
-  char string[51];
-  strcpy(string, "Programming is like building a multilingual puzzle");
+int main(void)
+{
+char string[52];
 
-  puts(string);
-  return 0;
+strcpy(string, "\"Programming is like building a multilingual puzzle");
+
+puts(string);
+return (0);
 }
