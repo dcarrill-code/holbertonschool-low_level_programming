dd80eaabb23fcf863000cdb1bef6a7e93b228ad7
dcarrill-code <3346@holbertonschool.com>
2021-06-17 18:33:26 +0000

    Fixing string lenght again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>/*This line import the stdio lib*/
 #include <string.h>/*This line import the string lib*/
 int main(void){
-  char string[50];
+  char string[51];
   strcpy(string, "Programming is like building a multilingual puzzle");
 
   puts(string);
