cfd92bbd30b6e45119a31357139f33694d198fe9
dcarrill-code <3346@holbertonschool.com>
2021-06-17 18:26:01 +0000

    First try

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+#include <string.h>/*This line import the string lib*/
+int main(void){
+  char string[20];
+  strcpy(string, "Programming is like building a multilingual puzzle")
+
+  puts(string);
+  return 0;
+}
