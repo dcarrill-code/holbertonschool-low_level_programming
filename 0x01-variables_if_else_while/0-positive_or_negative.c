56c93d3040d91d2cf3d92a91246701d9a7162ecc
dcarrill-code <3346@holbertonschool.com>
2021-06-18 22:19:05 +0000

    Fixing betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+* main - This main fuction can tell when a number is positive or negative
+*
+* Return: Returns a text telling if the number is positive or negative
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -10,16 +14,18 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-if(n>0)
+/* your code goes there */
+if (n > 0)
 {
-printf("%d is negative\n", n);
-}else if(n==0)
+printf("%d positive\n", n);
+}
+else if (n == 0)
 {
 printf("%d is zero\n", n);
-} else if(n<0) 
+}
+else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
-  return(0);
+return (0);
 }
