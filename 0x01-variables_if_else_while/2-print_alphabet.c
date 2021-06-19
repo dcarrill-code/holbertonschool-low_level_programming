52d3b116ba9ffbf59c1fb6a067b259e85ff9189b
dcarrill-code <3346@holbertonschool.com>
2021-06-19 17:57:49 +0000

    First try

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - This main function show the letters pf the alphabet
+*
+* Return: Returns all the letters of the alphabet
+*/
+
+int main(void)
+{
+char alphabet[30] = "abcdefghijklmnopqrstuvwxyz";
+putchar(alphabet, "\n");
+
+return (0);
+}
