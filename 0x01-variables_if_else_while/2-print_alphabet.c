d7645d031829c57c83666f4be6b29c5b12f6e477
dcarrill-code <3346@holbertonschool.com>
2021-06-19 18:25:27 +0000

    First try

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,13 @@
 
 int main(void)
 {
-char alphabet[30] = "abcdefghijklmnopqrstuvwxyz";
-putchar(alphabet, "\n");
+char alphabet;
 
+for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
+putchar(alphabet);
+
+}
+putchar('\n');
 return (0);
 }
