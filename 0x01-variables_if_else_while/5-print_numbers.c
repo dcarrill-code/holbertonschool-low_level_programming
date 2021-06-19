4dcf06dd8dedc450c6624f79f4db6862cdb50691
dcarrill-code <3346@holbertonschool.com>
2021-06-19 20:49:25 +0000

    First try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - A function that show some numbers
+*
+* Return: Returns the numbers from 0 to 9
+*/
+
+
+int main(void)
+{
+int num;
+
+for (num = 0 ; num < 10 ; num++)
+{
+putchar(num);
+
+}
+putchar('\n');
+}
