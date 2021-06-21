42dd2dda76510813a66d59b83c607402edd3673b
dcarrill-code <3346@holbertonschool.com>
2021-06-21 23:28:01 +0000

    First try

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -This main fuction show a text whit rite
+*
+* Return: This return a message and return 1
+*/
+
+int main(void)
+{
+
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19)", 50);
+
+return (1);
+}
