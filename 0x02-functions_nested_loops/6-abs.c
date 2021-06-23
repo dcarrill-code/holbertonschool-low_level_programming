276a08360f0bc43f233788535af2259d80eed210
dcarrill-code <3346@holbertonschool.com>
2021-06-23 03:10:16 +0000

    First try

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _abs - This function show the absolute valuo of a number
+* @r: this variable is the absolute value
+*
+* Return: Returns the absolute value of the variable
+*/
+
+
+
+int _abs(int r)
+{
+
+	if (r < 0)
+	{
+		r = r *-1;
+	}
+
+	return (r);
+}
