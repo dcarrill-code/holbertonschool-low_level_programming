0b4c817678208a400652a792a4ebcd5331d09e70
dcarrill-code <3346@holbertonschool.com>
2021-06-23 21:09:00 +0000

    First try

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* add - this function give the sum of 2 numbers
+* @a: the first variable to receive and give the result
+* @b: the second variable to receive for the sum
+*
+* Return: Returns the result of the sum
+*/
+
+
+
+int add(int a, int b)
+{
+
+a = a + b;
+
+return (a);
+
+}
