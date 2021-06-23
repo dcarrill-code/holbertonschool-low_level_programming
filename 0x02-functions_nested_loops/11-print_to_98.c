8eaba6ca88f2f0de71be7d07ae07a10362b38317
dcarrill-code <3346@holbertonschool.com>
2021-06-23 21:51:01 +0000

    Fixing betty style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 #include "stdio.h"
-
-
-
-
+/**
+ * print_to_98 -´Print n numbers in order until it reachs 98
+ * @n: the number to compare
+ *
+ * Return: Returns a series of number until 98
+*/
 
 void print_to_98(int n)
 {
@@ -14,7 +16,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 
-		if(n == 98)
+		if (n == 98)
 		{
 			printf("%d", n);
 			printf("\n");
@@ -28,7 +30,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 
-		if(n == 98)
+		if (n == 98)
 		{
 			printf("%d", n);
 			printf("\n");
@@ -36,7 +38,7 @@ void print_to_98(int n)
 		n++;
 		}
 	}
-	else if(n == 98)
+	else if (n == 98)
 	{
 		printf("%d", n);
 		printf("\n");
