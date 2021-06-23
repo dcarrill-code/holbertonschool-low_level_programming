e70173aee2f30acb059dcb829feb93986b499269
dcarrill-code <3346@holbertonschool.com>
2021-06-23 21:56:07 +0000

    Fixing the limit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,12 +14,16 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
+
 
 		if (n == 98)
 		{
 			printf("%d", n);
 			printf("\n");
+		}
+		else
+		{
+			printf("%d, ", n);
 		}
 		n--;
 		}
@@ -28,13 +32,13 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
-
 		if (n == 98)
 		{
 			printf("%d", n);
 			printf("\n");
 		}
+		else
+			printf("%d, ", n);
 		n++;
 		}
 	}
