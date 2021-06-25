7f38084cdb9ca878c149d3301e7df5fb00b34318
dcarrill-code <3346@holbertonschool.com>
2021-06-25 21:36:44 +0000

    First try

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* _isdigit - this function can tell when it gets a number
+* @c:is the variable to compare
+*
+* Return: returns 1 if c is a number between 0-9 and 0 if it is otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+
+
+}
