90dc2f083dbde25075f7af3525176a0e3d26d8cf
dcarrill-code <3346@holbertonschool.com>
2021-06-25 22:00:38 +0000

    First try

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include "stdio.h"
+/**
+* print_numbers - show the numbers from 0 to 9
+*
+* Return: returns the number in order from 0 to 9
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+
+}
