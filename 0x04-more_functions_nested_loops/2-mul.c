624905f59feed1d22d77ef7211ac9ccb10457efa
dcarrill-code <3346@holbertonschool.com>
2021-06-25 21:54:53 +0000

    First try

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* mul - this function multiplies two numbers
+* @a: this first variable to multiplie
+* @b: this secund variable to multiplie
+*
+* Return: Returns the result of the multiplication.
+*/
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
