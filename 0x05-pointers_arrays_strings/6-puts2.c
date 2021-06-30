99366ab6662fb37081f33c0aae9f2164f9c6ef84
dcarrill-code <3346@holbertonschool.com>
2021-06-30 00:48:55 +0000

    fixing betty style

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,35 @@
 #include "holberton.h"
 /**
+ * string_length - this function can take the length of a string
+ * @str: the pointer of the string to get the length
+ *
+ * Return: returns the length of the string
+ */
+
+int string_length(char *str)
+{
+	int con = 0;
+
+	while (*(str + con) != '\0')
+		con++;
+	return (con);
+
+}
+
+/**
 * puts2 - this function show the string skiping spaces
 * @str: this pointer is the string to show
 *
 */
 
-
-
-
 void puts2(char *str)
 {
-	int i;
+	int i, l;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	l = string_length(str);
+
+
+	for (i = 0 ; i < l - 1 ; i++)
 	{
 		putchar(str[i]);
 		i++;
