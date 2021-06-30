b5f354bc1283403472c1966cfa3c0a5d10283180
dcarrill-code <3346@holbertonschool.com>
2021-06-30 00:26:12 +0000

    Fixing betty style

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,26 @@
 #include "holberton.h"
-
-
+/**
+ * string_length - this function can tell the length of a string
+ * @s: the pointer to count
+ *
+ * Return: returns the length of the string
+ */
 
 int string_length(char *s)
 {
 	int con = 0;
-	while ( *(s + con) != '\0')
+
+	while (*(s + con) != '\0')
 		con++;
 
 	return (con);
 }
 
-
+/**
+ * rev_string - this function revert a string
+ * @s: the pointer to revert
+ *
+ */
 
 void rev_string(char *s)
 {
