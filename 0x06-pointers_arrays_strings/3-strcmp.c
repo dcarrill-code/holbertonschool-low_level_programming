ae20abd6bccb8185fe6de14b5f04422ba24066c9
dcarrill-code <3346@holbertonschool.com>
2021-07-03 03:05:30 +0000

    Fixing return

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,12 +14,9 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0 ; s1[i] && s2[i] ; ++i)
 	{
-		if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
-			continue;
-		else
-			break;
-	}
 
+
+	}
 	if (s1[i] == s2[i])
 		return (0);
 
