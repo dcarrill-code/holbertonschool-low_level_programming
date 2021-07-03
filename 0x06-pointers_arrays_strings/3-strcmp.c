4f135e42a7c06a27ad65ea7606b8f0512ef47e3b
dcarrill-code <3346@holbertonschool.com>
2021-07-03 02:57:28 +0000

    First try

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strcmp - this function compare two strings
+ * @s1: the first string to compare
+ * @s2: the second string to compare
+ *
+ * Return: returns 0 if they are the same return 1 if s1 grater than s2 and -1
+ * otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0 ; s1[i] && s2[i] ; ++i)
+	{
+		if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
+			continue;
+		else
+			break;
+	}
+
+	if (s1[i] == s2[i])
+		return (0);
+
+	if ((s1[i] | 32) < (s2[i] | 32))
+		return (-1);
+	return (1);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+
+
+
+
+
+
+
+
+
+
+
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if ((s[i] == 'a') && (s[i] == 'A')  (s[i] == 'e') && (s[i] == 'E'))
+		{
+
+		}
+	}
+
+}
