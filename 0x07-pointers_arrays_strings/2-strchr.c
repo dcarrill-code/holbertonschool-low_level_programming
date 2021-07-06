ac128c136334c8116c245ed024b5576dc4927b8e
dcarrill-code <3346@holbertonschool.com>
2021-07-06 02:32:10 +0000

    First try

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strchr - this function locate a character in a string and show the rest
+ * @s: the string to check
+ * @c: the character to find
+ *
+ * Return: returns a rest of the string from a character
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	int i, size;
+	char *p = s;
+
+	size = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		size++;
+	}
+
+	for (i = 0 ; i < size ; i++)
+	{
+		if (s[i] != c)
+			i++;
+		else
+			p++;
+	}
+
+	return (p);
+}
