f28c4abe79fb2062002a3244923fdb5f29624136
dcarrill-code <3346@holbertonschool.com>
2021-07-07 00:15:46 +0000

    First try

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strspn - this function count the first letters from accept that are in s
+ * @s: this string is the one to compare
+ * @accept: this string is the one that has the letter to compare whit s
+ *
+ * Return: returns an unsigned int whit the count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count;
+
+	count = 1;
+
+	for (i = 0; accept[i] != '\0' ; i++)
+	{
+		for (j = 0; s[j] != '\0' ; j++)
+
+		if (s[i] == accept[j])
+		{
+			count++;
+		}
+
+	}
+	return (count);
+
+}
