ff04aabe136df6a40d9fae2f0e0d702e6c2383d3
dcarrill-code <3346@holbertonschool.com>
2021-07-10 18:42:38 +0000

    First try

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * main - this main function print the numbers of parameters send
+ * @argc: the variable that has the number of parameters send with the program
+ * @argv: this variable is a string with the name of the parameters
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		argv++;
+	}
+	printf("%d\n", argc - 1);
+
+
+	return (0);
+
+}
