0c32c12bc4cbd09a47ed8c32e59c780952ab89f1
dcarrill-code <3346@holbertonschool.com>
2021-07-10 19:57:28 +0000

    First try

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * main - this function print the mult of 2 numbers
+ * @argc: this is the number of parameters send with the program
+ * @argv: this is a pointer to the names of the parameters sended
+ *
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, m;
+
+	m = 0;
+	if (argc > 1 && argc < 4)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+
+		m = num1 * num2;
+		printf("%d\n", m);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	return (0);
+}
