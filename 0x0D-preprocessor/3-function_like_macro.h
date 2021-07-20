5d6cb02dbeb449cf1c60b8d2e15c7996ef4260f2
dcarrill-code <3346@holbertonschool.com>
2021-07-20 18:55:07 +0000

    Fixing protect

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H_
-#define 3_FUNCTION_LIKE_MACRO_H_
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 #define ABS(X) (X < 0 ? (X * -1) : (X))
 
 #endif
