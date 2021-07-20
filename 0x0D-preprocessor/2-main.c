171c70e5d18f5b66535694fb4368a19e14821665
dcarrill-code <3346@holbertonschool.com>
2021-07-20 00:30:50 +0000

    First try

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This function print the name of the file
+ *
+ * Return:always return 0
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
