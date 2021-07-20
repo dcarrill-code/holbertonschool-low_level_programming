e8d28abcf3c0956544d3648f19c997a7e381367c
dcarrill-code <3346@holbertonschool.com>
2021-07-20 19:07:54 +0000

    Fixing betty style

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _3-FUNCTION_LIKE_MACRO_H_
-#define _3-FUNCTION_LIKE_MACRO_H_
+#ifndef __3-FUNCTION_LIKE_MACRO_H_
+#define __3-FUNCTION_LIKE_MACRO_H_
 #define ABS(X) (X < 0 ? (X * -1) : (X))
 
 #endif
