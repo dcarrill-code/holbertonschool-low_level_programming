c181a458e8b480bd723d8f0faf3fa5ffde146570
dcarrill-code <3346@holbertonschool.com>
2021-07-21 22:32:03 +0000

    Debugging

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,12 @@
 #include "dog.h"
 #include <stdlib.h>
-
-
+/**
+ * new_dog - This function creates a copy of the dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Return: a new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
@@ -15,33 +20,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (name[c1])
 		c1++;
 
-	d -> name = malloc(sizeof(char) * c1 + 1);
-	if(d->name == NULL)
+	d->name = malloc(sizeof(char) * c1 + 1);
+	if (d->name == NULL)
 	{
 		free(d->name);
 		free(d);
-		return(NULL);
+		return (NULL);
 	}
 
 	for (i = 0 ; i <= c1 ; i++)
-		d -> name[i] = name[i];
+		d->name[i] = name[i];
 
 	while (owner[c2])
 		c2++;
-	d -> owner = malloc(sizeof(char) * c2 + 1);
-	if (d -> owner == NULL)
+	d->owner = malloc(sizeof(char) * c2 + 1);
+	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d->owner);
 		free(d);
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0 ; i < c2 ; i++)
-		d -> owner[i] = owner[i];
+		d->owner[i] = owner[i];
 
 	if (d == NULL)
-		return(NULL);
+		return (NULL);
 
-	d -> age = age;
+	d->age = age;
 	return (d);
 }
