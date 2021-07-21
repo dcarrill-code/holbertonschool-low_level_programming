2e46272f68999d23ed80e9336a2fdf4d21b2dc72
dcarrill-code <3346@holbertonschool.com>
2021-07-21 17:06:49 +0000

    Fixing condition

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stddef.h>
 /**
  * print_dog - This funtion print the date of the struct dog
  * @d: the pointer to struct dog
@@ -11,17 +10,17 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("Name: (nill)");
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
 		if (d->age == 0)
-			printf("Age: (nill)");
+			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
-			printf("Owner: (nill)");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
