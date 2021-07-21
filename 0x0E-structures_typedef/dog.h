27981e82be09b005d0727506eeaa1e9cd66b84f5
dcarrill-code <3346@holbertonschool.com>
2021-07-21 15:56:52 +0000

    Adding new type to dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: This struct has the data of a dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog, dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
