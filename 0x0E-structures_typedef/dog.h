e66a13159d9c1e0933cf330b53c5ed19e1998938
dcarrill-code <3346@holbertonschool.com>
2021-07-21 08:07:00 +0000

    First try

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - The struct for the dog
+ * @name: the name ofthe dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: This struct has the data of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_ */
