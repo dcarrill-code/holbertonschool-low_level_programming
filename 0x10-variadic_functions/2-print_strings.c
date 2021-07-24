25f196a1dd1e3edf370b01f635f55014cb10fe8d
dcarrill-code <3346@holbertonschool.com>
2021-07-24 23:22:20 +0000

    First try

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - Print a list of strings
+ * @separator: is the separator send by the user
+ * @n: is the limit of the list of strings
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+	va_list ls;
+	unsigned int i;
+	char *str;
+
+	va_start(ls, n);
+
+	str = malloc(sizeof(char *) * n);
+	for (i = 0 ; i < n ; i++)
+	{
+		str = va_arg(ls, char*);
+
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+
+	}
+
+	va_end(ls);
+
+	putchar('\n');
+
+}
