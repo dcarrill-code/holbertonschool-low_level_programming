932e5da40586b091740ebb2f3557aa5e5acdb26a
dcarrill-code <3346@holbertonschool.com>
2021-07-24 18:35:59 +0000

    First try

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - This variadic function sum all the numbers it sends
+ * @n: the number of elements to sum
+ *
+ * Return: returns the sum of all the elements
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	va_list ls;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+
+
+	va_start(ls, n);
+
+	for(i = 0; i < n; i++)
+			sum += va_arg(ls, int);
+
+	va_end(ls);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+amdfiamfelkmwf
+
