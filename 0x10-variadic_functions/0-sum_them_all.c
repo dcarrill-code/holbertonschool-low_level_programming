e203dc146dbb4fee51669384de11f96d3250941c
dcarrill-code <3346@holbertonschool.com>
2021-07-24 19:33:54 +0000

    Debugging

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,17 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 
 	if (n == 0)
+	{
 		return (0);
-
+	}
 
 	va_start(ls, n);
 
-	for(i = 0; i < n; i++)
-			sum += va_arg(ls, int);
+	for (i = 0 ; i < n ; i++)
+	{
 
+		sum += va_arg(ls, int);
+	}
 	va_end(ls);
 
 	return (sum);
