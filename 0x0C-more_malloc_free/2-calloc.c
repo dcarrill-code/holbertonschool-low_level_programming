793dccfe26305b9c5758d86f42393b372e08bf34
dcarrill-code <3346@holbertonschool.com>
2021-07-27 19:14:18 +0000

    First try

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,32 @@
 #include "holberton.h"
-
-
-
-
-
-
-
+/**
+ * _calloc - This function is a new version of calloc
+ * @nmemb: this is the number of elements of an array
+ * @size: this is the size of the array
+ *
+ * Return: return the memory allocated or NULL if fails
+ */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
 	unsigned int i, n;
 	char *p, *q;
-	if (nmemb == 0|| size == 0)
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	n = nmemb * size;
 
-	if ((p = q = malloc(n)) != NULL)
+	p = q = malloc(n);
+
+	if (p != NULL)
 	{
 		for (i = 0; i < n; i++)
 			*p++ = 0;
 	}
 	else
-		return(NULL);
+		return (NULL);
 
 	return (q);
 
