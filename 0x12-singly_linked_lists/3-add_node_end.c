05a4441a578a57875525f21035d1f2fe2f1df333
dcarrill-code <3346@holbertonschool.com>
2021-07-31 05:18:07 +0000

    Fixing betty style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,9 +1,11 @@
 #include "lists.h"
-
-
-
-
-
+/**
+ * add_node_end - this function add a node to the tail of the linked list
+ * @head: this is the parameter pointing to the head of the linked list
+ * @str: this is the text to set in the node
+ *
+ * Return: returns a copy of the node;
+ */
 
 
 list_t *add_node_end(list_t **head, const char *str)
