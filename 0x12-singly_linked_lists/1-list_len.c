a3784f62430c96a316eb65d8b9a77f43bc49eab3
dcarrill-code <3346@holbertonschool.com>
2021-07-31 03:17:27 +0000

    First try

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,22 @@
-#include "list.h"
-
-
-
-
-
-
+#include "lists.h"
+/**
+ * list_len - this function print the number of nodes of a linked list
+ * @h: this points to the linked list head
+ *
+ *
+ * Return: returns the count of nodes
+ */
 
 size_t list_len(const list_t *h)
 {
 
+	size_t cont;
 
+	while (h != NULL)
+	{
+		cont++;
+		h = h->next;
+	}
 
-
+	return (cont);
 }
