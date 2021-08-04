203b0be76250bc9581eb8f7123bb2da0e00f0839
dcarrill-code <3346@holbertonschool.com>
2021-08-04 02:45:49 +0000

    First try

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
-
-
+/**
+ * print_listint - this function prints a linked list of ints
+ * @h: this pointers points to the number to print
+ *
+ *
+ * Return: returns the number of nodes
+ */
 
 size_t print_listint(const listint_t *h)
 {
