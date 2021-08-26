029e290b3a15d37252e17503a21736bcf655f9b5
dcarrill-code <3346@holbertonschool.com>
2021-08-26 15:33:59 -0500

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,39 +10,50 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest, neg = 0;
+	int largest, nega, negb, negc;
 
+	nega = negb = negc = 0;
 	if (a < 0)
 	{
 		a *= -1;
-		neg += 1;
+		nega += 1;
 	}
 	if (b < 0)
 	{
 		b *= -1;
-		neg += 1;
+		negb += 1;
 	}
 	if (c < 0)
 	{
 		c *= -1;
-		neg += 1;
+		negc += 1;
+
 	}
 
 	if (a >= b && b >= c)
 	{
 		largest = a;
+
+		if (nega == 1)
+			largest *= -1;
+
 	}
 
 	else if (b >= a && a >= c)
 	{
 		largest = b;
+		if (negb == 1)
+			largest *= -1;
+
 	}
 
 	else
+	{
 		largest = c;
+		if (negc == 1)
+			largest *= -1;
 
-	if (neg == 1)
-		return (largest *= -1);
+	}
 
 	return (largest);
 }
