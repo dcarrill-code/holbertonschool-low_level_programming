8ae6e0ce79e306f12ad8ef43ca0cdaecbfffc15d
dcarrill-code <3346@holbertonschool.com>
2021-08-26 18:09:25 -0500

    Catching up

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#define BUFFER 1024
+/**
+ * main - this file can copy the content of a file to another
+ *
+ * @argc: the number of arguments
+ * @argv: Value of the arguments
+ *
+ * Return: Allways return 0
+ */
+
+int main(int argc, char **argv)
+{
+	int file_from, file_to;
+	ssize_t reader, writer;
+	char buffer[BUFFER];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cd file_from file_to\n"), exit(97);
+
+	file_from = open(argv[1], O_RDONLY);
+
+	if (file_from == EOF)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]),
+					exit(98);
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == EOF)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+					exit(99);
+
+	do {
+		reader = read(file_from, buffer, BUFFER);
+		writer = write(file_to, buffer, reader);
+	} while (reader == BUFFER);
+
+	if (reader == EOF)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+
+	if (writer == EOF)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
+
+	if (close(file_from) == EOF)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from),
+			exit(100);
+
+	if (close(file_to) == EOF)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to),
+			exit(100);
+
+	return (0);
+}
