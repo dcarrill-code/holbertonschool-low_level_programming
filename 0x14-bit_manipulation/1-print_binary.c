da77db1540a6628849a391bb8f7398daae60dd8d
dcarrill-code <3346@holbertonschool.com>
2021-08-26 19:16:14 -0500

    Catching up

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,17 +2,16 @@
 /**
  * print_binary - this function converts a decimal number to binary
  * @n: this is the number to convert
- *
- * Return: returns the number in binary
  */
 
 
 void print_binary(unsigned long int n)
 {
-
-
-	if (n > 0)
+	if (n > 1)
 		print_binary(n >> 1);
 
-	putchar((n & 1) + '0');
+	if ((n & 1) == 1)
+		putchar('1');
+	else
+		putchar('0');
 }
